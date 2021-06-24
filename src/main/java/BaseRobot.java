4477910cffddd8807c1fda44c0c3922ff5203590
dvirkaza <dvirkazayof@edu.aac.ac.il>
2021-06-24 23:17:55 +0300

    מדפיס את הטקסט של הכתבה הראשית והכתבות המשניות

--- src/main/java/BaseRobot.java
+++ src/main/java/BaseRobot.java
@@ -4,15 +4,33 @@ public abstract class BaseRobot {
     private String rootWebsiteUrl;
 
     public BaseRobot(String rootWebsiteUrl) {
+
         this.rootWebsiteUrl = rootWebsiteUrl;
     }
 
     public String getRootWebsiteUrl() {
+
         return rootWebsiteUrl;
     }
 
     public void setRootWebsiteUrl(String rootWebsiteUrl) {
+
         this.rootWebsiteUrl = rootWebsiteUrl;
+    }
+    public String[]textToWord(String text){
+        String[]word=new String[text.length()];
+        int index=0;
+        word[index]="";
+        for (int i=0; i<text.length();i++){
+            if(text.charAt(i)!=' '){
+               word[index]+=text.charAt(i);
+            }
+            else {
+                index++;
+                word[index]="";
+            }
+        }
+        return word;
     }
 
     public abstract Map<String, Integer> getWordsStatistics();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,22 @@ public class Main
         try
         {
             Document website = Jsoup.connect("https://www.walla.co.il/").get();
-            Elements elements = website.getElementsByClass("main-taste");
-            System.out.println("found " + elements.size());
-            Element mainArticleElement=elements.get(0);
-            Elements h2s=mainArticleElement.getElementsByTag("article");
+            Elements elementsMainArticle=website.getElementsByClass("with-roof");
+            Element elementMainArticle=elementsMainArticle.get(0);
+            Elements h2=elementMainArticle.getElementsByTag("h2");
+            Element linkElementMain=h2.get(0).parent();
+            String linkMainArticle=linkElementMain.attr("href");
 
-            for (int i=0;i<h2s.size();i++) {
-                Element mainArticleTitle = h2s.get(i);
+            Document mainArticle=Jsoup.connect(linkMainArticle).get();
+            String textMainArticle=mainArticle.getElementsByClass("item-main-content").text();
+            System.out.println(textMainArticle);
+            System.out.println("----------------------");
+            Elements elementsSubArticles = website.getElementsByClass("main-taste");
+            Element subArticle=elementsSubArticles.get(0);
+            Elements article=subArticle.getElementsByTag("article");
+
+            for (int i=0;i<article.size();i++) {
+                Element mainArticleTitle = article.get(i);
                 Element linkElement = mainArticleTitle.parent();
                 String link = linkElement.attr("href");
                 Document news=Jsoup.connect(link).get();
