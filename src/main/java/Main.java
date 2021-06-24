5393b4942f8e01e043f02a453d18e2060b20fe1f
EladShapiro <eladshapiro@edu.aac.ac.il>
2021-06-24 11:19:47 +0300

    with  main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
-public class Main {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) {
+        try
+        {
+            Document website = Jsoup.connect("https://www.mako.co.il/").get();
+            Elements elements = website.getElementsByClass("mako_main_portlet_container");
+            System.out.println("found " + elements.size());
+            for (Element element : elements)
+            {
+                Element container = element.parent().parent();
+                String classValue = container.attr(("class"));
+                System.out.println(element.text());
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/WallaRobot.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class WallaRobot extends BaseRobot
+{
+
+    public WallaRobot(String rootWebsiteUrl) {
+        super(rootWebsiteUrl);
+    }
+
+    @Override
+    public Map<String, Integer> getWordsStatistics() {
+      Map<String,Integer> map=new HashMap<>();
+
+      //for ()
+        return null;
+
+    }
+
+    @Override
+    public int countInArticlesTitles(String text) {
+        return 0;
+    }
+
+    @Override
+    public String getLongestArticleTitle() {
+        return null;
+    }
+}
