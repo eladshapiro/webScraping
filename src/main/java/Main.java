dcd38127f09ce31c2df1b87dd7669367ea614af8
dvirkaza <dvirkazayof@edu.aac.ac.il>
2021-06-24 18:24:17 +0300

    מדפיס את הקישורים לכתבות המשניות

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import sun.awt.geom.AreaOp;
 
 import java.io.IOException;
 
@@ -10,15 +11,21 @@ public class Main
     public static void main(String[] args) {
         try
         {
-            Document website = Jsoup.connect("https://www.mako.co.il/").get();
-            Elements elements = website.getElementsByClass("mako_main_portlet_container");
+            Document website = Jsoup.connect("https://www.walla.co.il/").get();
+            Elements elements = website.getElementsByClass("main-taste");
             System.out.println("found " + elements.size());
-            for (Element element : elements)
-            {
-                Element container = element.parent().parent();
-                String classValue = container.attr(("class"));
-                System.out.println(element.text());
+            Element mainArticleElement=elements.get(0);
+            Elements h2s=mainArticleElement.getElementsByTag("article");
+
+            for (int i=0;i<h2s.size();i++) {
+                Element mainArticleTitle = h2s.get(i);
+                Element linkElement = mainArticleTitle.parent();
+                String link = linkElement.attr("href");
+                Document news=Jsoup.connect(link).get();
+                System.out.println(link);
             }
+
+
         }
         catch (IOException e)
         {
