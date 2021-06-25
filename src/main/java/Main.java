f45aa3d67abf863b0f4aafa4f497f7d28686eecd
EladShapiro <eladshapiro@edu.aac.ac.il>
2021-06-25 13:06:56 +0300

    put the wallqa robot in thr class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,40 +8,9 @@ import java.io.IOException;
 
 public class Main
 {
-    public static void main(String[] args) {
-        try
-        {
-            Document website = Jsoup.connect("https://www.walla.co.il/").get();
-            Elements elementsMainArticle=website.getElementsByClass("with-roof");
-            Element elementMainArticle=elementsMainArticle.get(0);
-            Elements h2=elementMainArticle.getElementsByTag("h2");
-            Element linkElementMain=h2.get(0).parent();
-            String linkMainArticle=linkElementMain.attr("href");
-
-            Document mainArticle=Jsoup.connect(linkMainArticle).get();
-            String textMainArticle=mainArticle.getElementsByClass("item-main-content").text();
-            System.out.println(textMainArticle);
-            System.out.println("----------------------");
-            Elements elementsSubArticles = website.getElementsByClass("main-taste");
-            Element subArticle=elementsSubArticles.get(0);
-            Elements article=subArticle.getElementsByTag("article");
-
-            for (int i=0;i<article.size();i++) {
-                Element mainArticleTitle = article.get(i);
-                Element linkElement = mainArticleTitle.parent();
-                String link = linkElement.attr("href");
-                Document news=Jsoup.connect(link).get();
-                String text=news.getElementsByClass("item-main-content").text();
-                System.out.println(text);
-                System.out.println("----------------------");
-
-            }
-
-
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+    public static void main(String[] args)
+    {
+        WallaRobot wallaRobot = new WallaRobot("https://www.walla.co.il/");
+        wallaRobot.getWordsStatistics();
     }
 }
--- src/main/java/WallaRobot.java
+++ src/main/java/WallaRobot.java
@@ -1,6 +1,11 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.util.HashMap;
 import java.util.Map;
-
+import java.io.IOException;
 public class WallaRobot extends BaseRobot
 {
 
@@ -9,13 +14,49 @@ public class WallaRobot extends BaseRobot
     }
 
     @Override
-    public Map<String, Integer> getWordsStatistics() {
+    public Map<String, Integer> getWordsStatistics()
+    {
       Map<String,Integer> map=new HashMap<>();
+        try
+        {
+            Document website = Jsoup.connect(super.getRootWebsiteUrl()).get();
+            Elements elementsMainArticle=website.getElementsByClass("with-roof");
+            Element elementMainArticle=elementsMainArticle.get(0);
+            Elements h2=elementMainArticle.getElementsByTag("h2");
+            Element linkElementMain=h2.get(0).parent();
+            String linkMainArticle=linkElementMain.attr("href");
 
-      //for ()
+            Document mainArticle=Jsoup.connect(linkMainArticle).get();
+            String textMainArticle=mainArticle.getElementsByClass("item-main-content").text();
+            System.out.println(textMainArticle);
+            System.out.println("----------------------");
+            Elements elementsSubArticles = website.getElementsByClass("main-taste");
+            Element subArticle=elementsSubArticles.get(0);
+            Elements article=subArticle.getElementsByTag("article");
+
+            for (int i=0;i<article.size();i++)
+            {
+                Element mainArticleTitle = article.get(i);
+                Element linkElement = mainArticleTitle.parent();
+                String link = linkElement.attr("href");
+                Document news=Jsoup.connect(link).get();
+                String text=news.getElementsByClass("item-main-content").text();
+                System.out.println(text);
+                System.out.println("----------------------");
+
+            }
+
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
         return null;
-
     }
+
+
+
 
     @Override
     public int countInArticlesTitles(String text) {
