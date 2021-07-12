6d9505f7ced254dc37d39f56f5450a61e4262c70
EladShapiro <eladshapiro@edu.aac.ac.il>
2021-07-12 19:52:21 +0300

    סידרתי את מה שמודפס שיהיה יותר מסודר

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,10 +17,10 @@ public class Main {
     public static void main(String[] args) throws IOException{
         Scanner scanner = new Scanner(System.in);
         int score = 0;
-        System.out.println("Which site would you like to crawl?");
-        System.out.println("1.mako");
-        System.out.println("2.walla");
-        System.out.println("3.ynet");
+        System.out.println("Which site would you like to crawl?-Eמter it's number:");
+        System.out.println("1.Mako");
+        System.out.println("2.Walla");
+        System.out.println("3.Ynet");
             int choice = scanner.nextInt();
             switch (choice) {
                 case mako:
@@ -43,16 +43,28 @@ public class Main {
 
     public static void levelOne(BaseRobot site){
     int score=0;
+        String word;
         System.out.println("Hint: The title of the longest article is: "+site.getLongestArticleTitle());
         System.out.println();
-        for (int i=0;i<5;i++) {
-            System.out.println("Enter a word that you think appears on the site");
-            String word=scanner.next();
-            if(site.getWordsStatistics().get(word)==null){
-                score+=0;
+        for (int i=0;i<5;i++)
+        {
+            if (i==0)
+            {
+                System.out.println("Enter a word that you think appears on the site  (you have 5 tries):");
+                 word = scanner.next();
             }
-            else score+=site.getWordsStatistics().get(word);
+            else
+            {
+                System.out.println("Enter another word:");
+                 word = scanner.next();
+                if (site.getWordsStatistics().get(word) == null)
+                {
+                    score += 0;
+                }
+                else
+                    score += site.getWordsStatistics().get(word);
+            }
         }
-        System.out.println("you'r score is:"+score);
+        System.out.println("\nyou'r score is:"+score);
     }
 }
