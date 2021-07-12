d93edf24445f173b85869e1208558ba96f815d42
dvirkaza <dvirkazayof@edu.aac.ac.il>
2021-07-12 16:24:46 +0300

    סידרתי קצת

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,56 +9,48 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner=new Scanner(System.in);
-        int score=0;
+    public static final Scanner scanner = new Scanner(System.in);
+    public static final int mako=1;
+    public static final int walla=2;
+    public static final int ynet=3;
+
+    public static void main(String[] args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
         System.out.println("Which site would you like to crawl?");
         System.out.println("1.mako");
         System.out.println("2.walla");
         System.out.println("3.ynet");
-    int choice = scanner.nextInt();
-        try {
-            switch (choice){
-                case 1:
-                    MakoRobot makoRobot=new MakoRobot();
-                    System.out.println(makoRobot.getLongestArticleTitle());
-                    for(int i=0;i<5;i++){
-                        String word=scanner.next();
-                        if(makoRobot.getWordsStatistics().get(word)==null){
-                            score+=0;
-                        }
-                        else score+=makoRobot.getWordsStatistics().get(word);
-                    }
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case mako:
+                    MakoRobot makoRobot = new MakoRobot();
+                    levelOne(makoRobot);
                     break;
-                case 2:
-                    WallaRobot wallaRobot=new WallaRobot();
-                    System.out.println(wallaRobot.getLongestArticleTitle());
-                    for (int i=0;i<5;i++) {
-                        String word=scanner.next();
-                        if(wallaRobot.getWordsStatistics().get(word)==null){
-                            score+=0;
-                        }
-                        else score+=wallaRobot.getWordsStatistics().get(word);
-                    }
+                case walla:
+                    WallaRobot wallaRobot = new WallaRobot();
+                   levelOne(wallaRobot);
                     break;
-                case 3:
-                    YnetRobot ynetRobot=new YnetRobot();
-                    System.out.println(ynetRobot.getLongestArticleTitle());
-                    for (int i=0;i<5;i++) {
-                        String word=scanner.next();
-                        if(ynetRobot.getWordsStatistics().get(word)==null){
-                            score+=0;
-                        }
-                        else score+=ynetRobot.getWordsStatistics().get(word);
-                    }
+                case ynet:
+                    YnetRobot ynetRobot = new YnetRobot();
+                    levelOne(ynetRobot);
                     break;
+
 
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+
+    public static void levelOne(BaseRobot site){
+    int score=0;
+        System.out.println("Hint: The title of the longest article is: "+site.getLongestArticleTitle());
+        for (int i=0;i<5;i++) {
+            String word=scanner.next();
+            if(site.getWordsStatistics().get(word)==null){
+                score+=0;
+            }
+            else score+=site.getWordsStatistics().get(word);
         }
         System.out.println(score);
-
-
     }
 }
